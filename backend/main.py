c515e196d2ed25a9a0822f6e923db3aae96e0ba8
amishaxb-0312 <amishaxb@gmail.com>
2026-08-31 11:19:06 +0530

    backend recommendations modified

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,23 @@ from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 import pandas as pd
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="SupplyPrescript AI",
     description="AI-powered supply chain prediction and decision optimization system",
     version="1.0.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load trained ML components
 model = joblib.load("ml/xgboost_delay_model.pkl")
