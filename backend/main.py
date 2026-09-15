f0427190a0258bf9671047b7baa3fda5226fb2ba
amishaxb-0312 <amishaxb@gmail.com>
2026-09-15 12:02:19 +0530

    production cors error fixing

--- backend/main.py
+++ backend/main.py
@@ -23,9 +23,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://supply-prescript-h1l7.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
